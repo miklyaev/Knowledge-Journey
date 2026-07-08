98a68b50a6a19f25064a426801916800c761fdf9
Miklyaev Sergey <miklyaev_sv@mail.ru>
2026-07-08 16:58:20 +0700

    t_add_user

--- backend/t_add_user.sql
+++ backend/t_add_user.sql
@@ -1,3 +1,3 @@
-CREATE USER IF NOT EXISTS 'mikser'@'%' IDENTIFIED BY 'Tornado_Mikser_5';
-GRANT ALL PRIVILEGES ON *.* TO 'mikser'@'%' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS 'mikser'@'%' IDENTIFIED WITH mysql_native_password BY 'Tornado_Mikser_5';
+GRANT ALL PRIVILEGES ON JourneyDb.* TO 'mikser'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
